94ee4deb4e2cda99c97cdee47a85ca10b21d1611
채명준 <cmj9597@naver.com>
2022-11-02 11:59:10 +0900

    Setup Boilderplate - nextjs - wagmi + connectkit - prettier - tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+import {
+  ConnectKitButton,
+  ConnectKitProvider,
+  getDefaultClient,
+} from 'connectkit'
+import { createClient, WagmiConfig } from 'wagmi'
+
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID
+
+const client = createClient(
+  getDefaultClient({
+    appName: 'Your App Name',
+    infuraId,
+  })
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+        <ConnectKitButton />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  )
 }
